dcc3feea391761227d2d3045fd01399cd6fdb9ad
Tong Shu Li <tongli@scripps.edu>
2016-03-31 16:04:14 -0700

    Create worm plot notebook
    Create notebook containing first version of drift plot functions. The
    notebook tries to replicate the drift plots seen in PMID 26623667.
    
    P-values for each unique pairwise group of genes are displayed as a
    heatmap.

--- src/drift_plots.py
+++ src/drift_plots.py
@@ -2,6 +2,7 @@
 """
 
 import pandas as pd
+import seaborn as sns
 
 def plot_drift_raw(data, time_col, gene_col, drift_col, title = None):
     """Plot the drift values of a group of individual genes.
@@ -17,3 +18,23 @@ def plot_drift_raw(data, time_col, gene_col, drift_col, title = None):
     graph.legend(loc = "center left", bbox_to_anchor = (1, 0.5))
 
     return graph
+
+def plot_drift_box(data, time_col, drift_col, gene_col,
+    groupby = None, split = False):
+    """Plot the drift values of a group of genes as a boxplot.
+
+    Different choices:
+    1. Single figure with one group
+    2. Paired boxplots across one figure
+    3. Paired boxplots with separate subfigures
+    """
+    if groupby is not None and split:
+        fig = sns.FacetGrid(data, col = groupby)
+        fig = fig.map(sns.boxplot, time_col, drift_col, showfliers = False)
+    else:
+        fig = sns.boxplot(
+            data = data, x = time_col, y = drift_col, showfliers = False,
+            hue = groupby
+        )
+
+    return fig
