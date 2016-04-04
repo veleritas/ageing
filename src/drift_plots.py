f8ce7c00765ed9d5ebdbdcc7f66e4a1fe910b135
Tong Shu Li <tongli@scripps.edu>
2016-04-04 12:14:37 -0700

    Add warning for raw drift plotter
    Warn user when they try to plot more than 20 genes using the raw drift
    plotting function.

--- src/drift_plots.py
+++ src/drift_plots.py
@@ -1,21 +1,31 @@
 """Functions for creating transcriptional drift plots.
 """
-
 import pandas as pd
 import seaborn as sns
+import warnings
 
 def plot_drift_raw(data, time_col, gene_col, drift_col, title = None):
     """Plot the drift values of a group of individual genes.
 
     Recommend no more than ~20 genes plotted to avoid overloading the legend.
     """
+    MAX_GENES = 20
     assert isinstance(data, pd.DataFrame), "No dataframe provided!"
+    if len(set(data[gene_col])) > MAX_GENES:
+        warnings.warn("Plotting more than {} genes is not recommended".format(MAX_GENES))
 
     sub = data.pivot(index = time_col, columns = gene_col, values = drift_col)
 
     graph = sub.plot(kind = "line", title = title)
     graph.set_ylabel("Drift value")
     graph.legend(loc = "center left", bbox_to_anchor = (1, 0.5))
+
+    graph.annotate(
+        r'$N = {}$'.format(len(set(data[gene_col]))),
+        xy = (1, 1), xycoords = "axes fraction", fontsize = 16,
+        xytext = (5, -10), textcoords = "offset points",
+        ha = "left", va = "top"
+    )
 
     return graph
 
@@ -35,6 +45,13 @@ def plot_drift_box(data, time_col, drift_col, gene_col,
         fig = sns.boxplot(
             data = data, x = time_col, y = drift_col, showfliers = False,
             hue = groupby
+        )
+
+        fig.annotate(
+            r'$N = {}$'.format(len(set(data[gene_col]))),
+            xy = (1, 1), xycoords = "axes fraction", fontsize = 16,
+            xytext = (5, -10), textcoords = "offset points",
+            ha = "left", va = "top"
         )
 
     return fig
